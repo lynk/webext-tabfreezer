a1857daeab4f74aca85b39ea1ad937e364e54d19
lynk <dev@lynk.de>
2018-10-05 14:13:39 +0200

    Codebase changed from mozillas promise API to chromes callback API

--- tabfreezer_bg.js
+++ tabfreezer_bg.js
@@ -2,9 +2,7 @@
 
 
 var browser = (function () {
-    return window.msBrowser ||
-        window.browser ||
-        window.chrome;
+    return window.browser || window.chrome;
 })();
 
 var TF = {
@@ -19,7 +17,7 @@ var TF = {
             },
             title: 'TURN OFF Freezing',
             action: 'addUrl',
-            nextState: 'off'
+            nextState: 'on'
         },
         off: {
             icon: {
@@ -37,27 +35,31 @@ var TF = {
         }
     },
 
+    /**
+     *
+     *
+     * @param tabId
+     * @param changeInfo
+     * @param tabInfo
+     */
     onUpdatedTab: function (tabId, changeInfo = '', tabInfo = '') {
 
         // TODO: I want this to run only once
-
         //check if host is in storage
-        browser.storage.local.get("urls").then(item => {
+        browser.storage.local.get(["urls"], function (result) {
 
             const currentHost = TF.getHostFromUrl(tabInfo.url);
 
-            if (item.urls !== undefined && item.urls.includes(currentHost)) {
+            if (result.urls !== undefined && result.urls.includes(currentHost)) {
 
                 // Toggle button state on
                 TF.toggleBrowserButton(tabId, TF.browserButtonStates.off.nextState);
                 TF.startTabListening();
             }
             else {
-                // obsolete?
-                //toggle button state off
-                TF.toggleBrowserButton(tabId, TF.browserButtonStates.on.nextState);
+                //TF.toggleBrowserButton(tabId, TF.browserButtonStates.on.nextState);
             }
-        });
+        })
     },
 
 
@@ -82,48 +84,50 @@ var TF = {
         });
     },
 
+    /**
+     * This handles the click on the browser button.
+     * The title of the button acts as state
+     *
+     * @param tab
+     */
     handleBrowserClick: function (tab) {
 
-        browser.browserAction.getTitle({tabId: tab.id})
-            .then(title => {
+        browser.browserAction.getTitle({tabId: tab.id}, function (result) {
 
-                const defaultState = TF.browserButtonStates.defaultState;
+            const defaultState = TF.browserButtonStates.defaultState;
 
-                // get next state
-                let nextState;
-                if (title == TF.browserButtonStates[defaultState].title) {
-                    nextState = TF.browserButtonStates[defaultState].nextState;
-                }
-                else {
-                    nextState = defaultState;
-                }
+            // get next state
+            let nextState;
 
-                // Toogle buttons
-                TF.updateBrowserButtons(TF.getHostFromUrl(tab.url), nextState);
+            if (result == TF.browserButtonStates[defaultState].title) {
+                nextState = TF.browserButtonStates[defaultState].nextState;
+            }
+            else {
+                nextState = defaultState;
+            }
 
-                // Call add or remove url function
-                TF[TF.browserButtonStates[nextState].action](tab);
+            // Toogle buttons
+            TF.updateBrowserButtons(TF.getHostFromUrl(tab.url), nextState);
 
-                // Toggle listening for new tabs
-                if (nextState != defaultState) {
-                    TF.startTabListening();
-                }
+            // Call add or remove url function
+            TF[TF.browserButtonStates[nextState].action](tab);
 
-            });
+            // Toggle listening for new tabs
+            if (nextState != defaultState) {
+                TF.startTabListening();
+            }
+        })
     },
 
     updateBrowserButtons: function (host, nextState) {
 
         // Get all tabs with this host
-        browser.tabs.query({
-            url: "*://" + host + "/*"
-        }).then(tabs => {
-
+        browser.tabs.query({url: "*://" + host + "/*"}, function (tabs) {
             // Toggle plugin icon on all tabs
             Object.keys(tabs).forEach(function (key) {
                 TF.toggleBrowserButton(tabs[key].id, nextState)
             });
-        });
+        })
     },
 
     startTabListening: function () {
@@ -137,6 +141,8 @@ var TF = {
      * @param command
      */
     onKeyCommand: function (command) {
+
+
 
         if (command == 'freezer-override') {
 
@@ -162,42 +168,41 @@ var TF = {
     catchCreated: function (tabs) {
 
         // Don't close tab if override active
+        console.log(TF.freezerOverrideActive);
         if (TF.freezerOverrideActive === true) {
             TF.endOverride();
         }
         else {
-
             // Check if the opener tab of the new tab is freezed (by the icon title)
-            browser.browserAction.getTitle({tabId: tabs.openerTabId}).then(title => {
-
+            browser.browserAction.getTitle({tabId: tabs.openerTabId}, function (title) {
                 if (title == TF.browserButtonStates.on.title) {
 
                     // Remove listener temporarily to prevent core errors
                     //browser.tabs.onUpdated.removeListener(TF.onUpdatedTab);
-
                     TF.closeTab(tabs.id);
 
                     TF.incremBadge(tabs.openerTabId);
                 }
-            }, TF.onError);
-        } //else
+            })
+        }
+
+
     },
 
     incremBadge: function (tabId) {
 
-        browser.browserAction.getBadgeText({tabId: tabId}).then(text => {
-
+        browser.browserAction.getBadgeText({tabId: tabId}, function (text) {
             if (text !== '') {
                 browser.browserAction.setBadgeText({tabId: tabId, text: (++text).toString()});
             } else {
                 browser.browserAction.setBadgeText({tabId: tabId, text: '1'});
             }
-        });
+        })
     },
 
     closeTab: function (tabId) {
 
-        browser.tabs.remove(tabId);
+        browser.tabs.remove(tabId, TF.onError);
         //re-add listener
         //browser.tabs.onUpdated.addListener(TF.onUpdatedTab);
     },
@@ -209,26 +214,23 @@ var TF = {
 
     addUrl: function (tab) {
 
-        browser.storage.local.get("urls")
-            .then(function (item) {
+        browser.storage.local.get(["urls"], function (result) {
+            const host = TF.getHostFromUrl(tab.url);
 
-                const host = TF.getHostFromUrl(tab.url);
+            result.urls.push(host);
+            const newUrls = [...new Set(result.urls.map(a => a))];
 
-                item.urls.push(host);
-                const newUrls = [...new Set(item.urls.map(a => a))];
-
-                browser.storage.local.set({urls: newUrls});
-
-            }, TF.onError);
+            browser.storage.local.set({urls: newUrls});
+        });
     },
 
     removeUrl: function (tab) {
 
-        browser.storage.local.get("urls").then(item => {
-
+        browser.storage.local.get(["urls"], function (result) {
             const currentHost = TF.getHostFromUrl(tab.url);
-            const newUrls = item.urls.filter(host => host != currentHost);
+            const newUrls = result.urls.filter(host => host != currentHost);
             browser.storage.local.set({urls: newUrls});
+
         });
     },
 
@@ -247,19 +249,20 @@ var TF = {
 
     /**
      * On install: set up storage, counter
+     * counter currently unused
      */
     onInstall: function (details) {
 
         switch (details.reason) {
             case 'install':
 
-                browser.storage.local.get('urls').then(results => {
+                browser.storage.local.get(['urls'], function (results) {
 
                     if (Object.keys(results).length === 0) {
                         browser.storage.local.set({
                             urls: [],
                             counter: 0
-                        }).then(TF.onError, TF.onError);
+                        });
                     }
                 });
                 break;
@@ -267,7 +270,11 @@ var TF = {
     },
 
     onError: function (error) {
-        console.log('Error: ' + error);
+
+        if (browser.runtime.lastError) {
+            console.log(browser.runtime.lastError);
+
+        }
     }
 };
 
