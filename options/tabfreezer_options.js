8153eebe96ddd1a2aff5c5ded612c288edc5d6b3
lynk <dev@lynk.de>
2018-10-07 23:46:37 +0200

    Commends and CSS added

--- options/tabfreezer_options.js
+++ options/tabfreezer_options.js
@@ -5,12 +5,16 @@ var browser = (function () {
 
 document.addEventListener("DOMContentLoaded", function () {
 
+    // add click listenr
     document.getElementById('tf-delete-submit').addEventListener('click', resetStorage);
 
     getNumberOfUrls();
 });
 
 
+/**
+ * Print number of stored urls on options page
+ */
 function getNumberOfUrls() {
 
     browser.storage.local.get(["urls"], function (result) {
@@ -18,13 +22,14 @@ function getNumberOfUrls() {
     });
 }
 
+/**
+ * Reset urls storage value to empty array
+ * Message background script to toggle off all active buttons
+ */
 function resetStorage() {
 
     browser.storage.local.set({urls: []}, function () {
         document.getElementById("tf-counter").innerHTML = '0';
         browser.runtime.sendMessage({is: 'reset'});
     });
-
-
-
 }
